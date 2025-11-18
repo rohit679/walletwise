75f20be75c3347a93b42c001faa8c2ed235711ef
rohit679 <rohitprasad25061999@gmail.com>
2025-11-18 21:09:38 +0530

    token & secrets added in the configuration

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -1,4 +1,8 @@
 export const getSecret = () => ({
   port: process.env.PORT || 8000,
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/walletwise',
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
 });
