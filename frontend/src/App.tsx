3204f4d09f3d902d912d53dff9e14bc57ca2fb02
rohit679 <rohitprasad25061999@gmail.com>
2025-11-25 00:12:00 +0530

    frontend code basic setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center items-center text-xl'>Walletwise</div>
+  )
+}
+
+export default App
