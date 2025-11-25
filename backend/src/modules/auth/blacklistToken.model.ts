43142e7fd9ff8ed246512c181654dc4ba6e2a1f4
rohit679 <rohitprasad25061999@gmail.com>
2025-11-25 19:58:02 +0530

    blacklisttoken model created for invalidating the JWT token

---
+++ backend/src/modules/auth/blacklistToken.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, InferSchemaType } from 'mongoose';
+
+const blackListTokenSchema = new Schema(
+  {
+    token: { type: String, required: true, unique: true },
+    blacklistedAt: { type: Date, default: Date.now },
+    expiresAt: { type: Date, required: true }
+  },
+  { strict: false, timestamps: true }
+);
+
+blackListTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+export type BlackListTokenDoc = InferSchemaType<typeof blackListTokenSchema>;
+export const BlackListTokenModel = mongoose.model('blacklist_token', blackListTokenSchema);
