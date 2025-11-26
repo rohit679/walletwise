7f1a8df29b160aa8b4c7164eda7b6139a505e1a8
rohit679 <rohitprasad25061999@gmail.com>
2025-11-27 00:39:22 +0530

    axios interceptor configured

---
+++ frontend/src/api/apiClient.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 15000,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
