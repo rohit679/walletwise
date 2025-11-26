f19967b62f019d44616f4707acc2ee2c19379a10
rohit679 <rohitprasad25061999@gmail.com>
2025-11-27 00:38:48 +0530

    api endpoint configuration file added

---
+++ frontend/src/common/apiEndpoint.ts
@@ -0,0 +1,6 @@
+export const apiEndpoint = {
+    register: "/auth/register",
+    login: "/auth/login",
+    forgotPassword: "/auth/forgot-password",
+    resetPassword: "/auth/reset-password",
+};
